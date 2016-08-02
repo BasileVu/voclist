7e43232254867060c71e55d1cebaea89a54f126e
Basile Vu <basile.vu@gmail.com>
2016-08-03 01:17:58 +0200

    using flask-sqlalchemy

--- server/launch.py
+++ server/launch.py
@@ -2,8 +2,9 @@
 
 import os
 
-from server.models import *
-from server.session import DB_FOLDER, engine, Session
+from server.models import DB_FOLDER
+from server.models import EntrySet, Entry, Tag
+from server.models import db
 
 __author__ = "Basile Vu <basile.vu@gmail.com>"
 
@@ -12,10 +13,10 @@ if __name__ == "__main__":
     if not os.path.exists(DB_FOLDER):
         os.makedirs(DB_FOLDER)
 
-    Base.metadata.create_all(engine, checkfirst=True)
+    db.create_all()
 
-    session = Session()
-    e1 = Entry(value="test1", translation="t1")
+    l1 = EntrySet(name="jp-en")
+    e1 = Entry(value="test1", translation="t1", entry_set=l1)
     e2 = Entry(value="test2", translation="t2")
     tag1 = Tag(value="testing1")
     tag2 = Tag(value="testing2")
@@ -26,7 +27,7 @@ if __name__ == "__main__":
     print("related to 'testing1': ")
 
     for e in tag1.entries:
-        print(e.value)
+        print(e)
         print(" its tags are the following: ")
         for t in e.tags:
-            print("  " + t.value)
+            print("  " + str(t))
--- server/models.py
+++ server/models.py
@@ -1,36 +1,51 @@
-from sqlalchemy import ForeignKey, Column, Table
-from sqlalchemy import Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 __author__ = "Basile Vu <basile.vu@gmail.com>"
 
-Base = declarative_base()
+DB_FOLDER = "db"
+DB_NAME = "db.db"
 
-entry_tag = Table("entry_tag", Base.metadata,
-                  Column("Entry_id", Integer, ForeignKey("entries.id")),
-                  Column("Tag_id", Integer, ForeignKey("tags.id"))
+app = Flask(__name__)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/db.db"
+db = SQLAlchemy(app)
+
+
+entry_tag = db.Table("entry_tag",
+                     db.Column("Entry_id", db.Integer, db.ForeignKey("entries.id")),
+                     db.Column("Tag_id", db.Integer, db.ForeignKey("tags.id"))
                  )
 
-class Language(Base):
-    __tablename__ = "languages"
-    id = Column(Integer, primary_key=True)
-    value = Column(String, nullable=False)
+
+class EntrySet(db.Model):
+    __tablename__ = "entry_set"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return "<EntrySet(name='%s')>" % self.name
 
 
-class Entry(Base):
+class Entry(db.Model):
     __tablename__ = "entries"
 
-    id = Column(Integer, primary_key=True)
-    value = Column(String, nullable=False)
-    translation = Column(String, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.String, nullable=False)
+    translation = db.Column(db.String, nullable=False)
 
-    language_id = Column(Integer, ForeignKey('languages.id'))
-    tags = relationship("Tag", secondary=entry_tag, backref="entries")
+    entry_set = db.Column(db.Integer, db.ForeignKey('entry_set.id'))
+    tags = db.relationship("Tag", secondary=entry_tag, backref="entries")
+
+    def __repr__(self):
+        return "<Entry(value='%s', translation='%s', entry_set='%s')>" % (self.value, self.translation, self.entry_set)
 
 
-class Tag(Base):
+class Tag(db.Model):
     __tablename__ = "tags"
 
-    id = Column(Integer, primary_key=True)
-    value = Column(String, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return "<Tag(value='%s')>" % self.value
