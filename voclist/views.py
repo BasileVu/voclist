64cf4b6ea5a6ef6be1f7cc9db320ce6268b956c5
Basile Vu <basile.vu@gmail.com>
2016-08-05 16:09:34 +0200

    Page to add voclists

--- voclist/views.py
+++ voclist/views.py
@@ -7,6 +7,25 @@ from voclist.models import Voclist, Entry, Tag
 
 @app.route("/")
 def index():
+    return render_template("index.html", voclists=Voclist.query.all())
+
+
+@app.route("/voclists/", methods=["POST"])
+def create_voclist():
+    voclist = Voclist(
+        name="%s-%s" % (request.form["lang-left"], request.form["lang-right"])
+    )
+
+    print(voclist)
+
+    db.session.add(voclist)
+    db.session.commit()
+
+    return "created" + voclist.name
+
+
+@app.route("/entries/", methods=["GET"])
+def entries():
     voclist = Voclist.query.filter_by(name="jp-en").first()  # FIXME
     print(voclist)
     return render_template("entries.html", voclist=voclist)
