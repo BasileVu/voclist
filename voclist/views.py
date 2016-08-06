d862d992dedb4776e36181c297e45774b6469c2d
Basile Vu <basile.vu@gmail.com>
2016-08-06 08:28:09 +0200

    Basic error handling

--- voclist/views.py
+++ voclist/views.py
@@ -1,4 +1,4 @@
-from flask import redirect, render_template, request, url_for
+from flask import abort,    redirect, render_template, request, url_for
 
 from voclist import app, db
 from voclist.models import Voclist, Entry, Tag
@@ -11,39 +11,50 @@ def index():
 
 @app.route("/voclists/", methods=["POST"])
 def create_voclist():
-    voclist = Voclist(
-        language_left = request.form["language-left"],
-        language_right = request.form["language-right"]
-    )
+    language_left = request.form["language-left"]
+    language_right = request.form["language-right"]
 
-    print(voclist)
+    if language_left == "" or language_right == "":
+        abort(401) # FIXME error code for invalid parameter or action
+
+    voclist = Voclist(
+        language_left=language_left ,
+        language_right=language_right
+    )
 
     db.session.add(voclist)
     db.session.commit()
 
-    return redirect("/entries/%s/" % str(voclist.id))  # FIXME url_for
+    return redirect("/entries/%d/" % voclist.id)  # FIXME url_for
 
 
 @app.route("/entries/<int:voclist_id>/", methods=["GET"])
 def entries(voclist_id):
     voclist = Voclist.query.get(voclist_id)
-    print(voclist)
+
+    if voclist is None:
+        abort(404)
+
     return render_template("entries.html", voclist=voclist)
 
 
 @app.route("/entries/", methods=["POST"])
 def create_entry():
+
+    word = request.form["word"]
+    translation = request.form["translation"]
+    voclist_id = 1  # FIXME
+
+    if word == "" or translation == "":
+        abort(401) # FIXME error code for invalid parameter or action
+
     entry = Entry(
-        word=request.form["word"],
-        translation=request.form["translation"],
-        voclist_id=1  # FIXME
+        word=word,
+        translation=translation,
+        voclist_id=voclist_id
     )
-
-    #entry.tags.append(Tag(value="tag"))  # FIXME
-
-    print(entry)
 
     db.session.add(entry)
     db.session.commit()
 
-    return redirect("/entries/1")  # FIXME url_for + id
+    return redirect("/entries/%d/" % voclist_id)  # FIXME url_for
