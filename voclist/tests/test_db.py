cdb36e886c0016c7e3bc0884a8ec13806ba71abc
Basile Vu <basile.vu@gmail.com>
2016-08-24 18:49:28 +0200

    Testing tag unicity

--- voclist/tests/test_db.py
+++ voclist/tests/test_db.py
@@ -1,3 +1,6 @@
+import sqlalchemy.exc
+import sqlite3
+
 from flask_testing import TestCase
 
 from voclist import *
@@ -7,22 +10,22 @@ from voclist.models import *
 __author__ = "Basile Vu <basile.vu@gmail.com>"
 
 
-def add_voclist():
-    v = Voclist(language_left="a", language_right="b")
+def add_voclist(language_left, language_right):
+    v = Voclist(language_left=language_left, language_right=language_right)
     db.session.add(v)
     db.session.commit()
     return v
 
 
-def add_tag():
-    t = Tag(value="test")
+def add_tag(value):
+    t = Tag(value=value)
     db.session.add(t)
     db.session.commit()
     return t
 
 
-def add_entry(voclist, tag=None):
-    e = Entry(word="word", translation="translation", voclist_id=voclist.id)
+def add_entry(word, translation, _voclist, tag=None):
+    e = Entry(word=word, translation=translation, voclist_id=_voclist.id)
     if tag is not None:
         e.tags.append(tag)
     db.session.add(e)
@@ -44,7 +47,7 @@ class DBTest(TestCase):
         db.drop_all()
 
     def test_voclist_creation(self):
-        v = add_voclist()
+        v = add_voclist("a", "b")
 
         self.assertEqual(Voclist.query.count(), 1)
 
@@ -54,7 +57,7 @@ class DBTest(TestCase):
         self.assertEqual(v_get.language_right, "b")
 
     def test_tag_creation(self):
-        t = add_tag()
+        t = add_tag("test")
 
         self.assertEqual(Tag.query.count(), 1)
 
@@ -62,39 +65,49 @@ class DBTest(TestCase):
         self.assertEqual(t_get, t)
         self.assertEqual(Tag.query.get(1).value, "test")
 
+    def test_unique_tag_name_creation(self):
+        add_tag("t")
+        self.assertRaises(sqlalchemy.exc.IntegrityError, add_tag, "t")
+
+    def test_unique_tag_name_update(self):
+        add_tag("t")
+        t2 = add_tag("t2")
+        t2.value = "t"
+        self.assertRaises(sqlalchemy.exc.IntegrityError, db.session.commit)
+
     def test_entry_creation(self):
-        v = add_voclist()
-        e = add_entry(v)
+        v = add_voclist("a", "b")
+        e = add_entry("w", "t", v)
 
         self.assertEqual(Entry.query.count(), 1)
 
         e_get = Entry.query.get(1)
         self.assertEqual(e_get, e)
 
-        self.assertEqual(e_get.word, "word")
-        self.assertEqual(e_get.translation, "translation")
+        self.assertEqual(e_get.word, "w")
+        self.assertEqual(e_get.translation, "t")
         self.assertEqual(e_get.voclist_id, v.id)
 
     def test_entry_with_tags_creation(self):
-        t = add_tag()
-        v = add_voclist()
-        e = add_entry(v, t)
+        t = add_tag("t")
+        v = add_voclist("a", "b")
+        e = add_entry("w", "t", v, t)
 
         self.assertEqual(Entry.query.count(), 1)
 
         e_get = Entry.query.get(1)
         self.assertEqual(e_get, e)
 
-        self.assertEqual(e_get.word, "word")
-        self.assertEqual(e_get.translation, "translation")
+        self.assertEqual(e_get.word, "w")
+        self.assertEqual(e_get.translation, "t")
         self.assertEqual(len(e_get.tags), 1)
         self.assertEqual(e_get.tags[0], t)
         self.assertEqual(e_get.voclist_id, v.id)
 
     def test_tag_deletion(self):
-        t = add_tag()
-        v = add_voclist()
-        e = add_entry(v, t)
+        t = add_tag("t")
+        v = add_voclist("a", "b")
+        e = add_entry("w", "t", v, t)
 
         db.session.delete(t)
         db.session.commit()
@@ -102,10 +115,10 @@ class DBTest(TestCase):
         self.assertEqual(len(e.tags), 0)
 
     def test_entry_deletion(self):
-        t = add_tag()
-        v = add_voclist()
-        e = add_entry(v, t)
-        e2 = add_entry(v, t)
+        t = add_tag("t")
+        v = add_voclist("a", "b")
+        e = add_entry("w", "t", v, t)
+        e2 = add_entry("w2", "t2", v, t)
 
         e.remove_tags()
         db.session.delete(e)
@@ -121,10 +134,10 @@ class DBTest(TestCase):
         db.session.commit()
 
     def test_voclist_deletion(self):
-        t = add_tag()
-        v = add_voclist()
-        add_entry(v, t)
-        add_entry(v, t)
+        t = add_tag("t")
+        v = add_voclist("a", "b")
+        add_entry("w", "t", v, t)
+        add_entry("w2", "t2", v, t)
 
         for entry in v.entries:
             entry.remove_tags()
