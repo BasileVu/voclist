60344cde4d6c1c8397538178f4c09d7cb42bca27
Basile Vu <basile.vu@gmail.com>
2016-09-22 00:16:02 +0200

    Tests for /

--- voclist/tests/test_views.py
+++ voclist/tests/test_views.py
@@ -20,7 +20,22 @@ class ViewsTest(TestCase):
         db.session.remove()
         db.drop_all()
 
+    def test_root_breadcrumbs(self):
+        r = self.client.get("/")
+        self.assert200(r)
+        assert '<ol class="breadcrumb">' in str(r.data)
+        assert '<li class="active">Voclists</li>' in str(r.data)
+
     def test_empty_voclist(self):
         r = self.client.get("/")
         self.assert200(r)
-        assert "<p>No voclist found yet. Please create one.</p>" in str(r.data)
+        assert '<p>No voclist found yet. Please create one.</p>' in str(r.data)
+
+    def test_existing_voclist(self):
+        db.session.add(Voclist(language_left="a", language_right="b"))
+        db.session.commit()
+
+        r = self.client.get("/")
+        self.assert200(r)
+        print(r.data)
+        assert '<option voclist-id="1">a - b</option>' in str(r.data)
